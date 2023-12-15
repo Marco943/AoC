e645362e8d296178bf552069b703fd6c4910aadd
Marco <marco@sucessoemvendas.com.br>
2023-12-15 13:12:53 -0300

    parte 2 dia 15 resolvido

--- Day15/hash.py
+++ Day15/hash.py
@@ -10,10 +10,29 @@ def hash(s: str) -> int:
     return value
 
 
-sequence = sum(
-    map(
-        hash,
-        open("Day15/hash.txt", "r").read().strip().replace("\n", "").split(","),
-    )
-)
-print(sequence)
+sequence = open("Day15/hash.txt", "r").read().strip().replace("\n", "").split(",")
+
+
+boxes = {i: {} for i in range(256)}
+
+for i, cur_value in enumerate(sequence):
+    if "-" in cur_value:
+        lens = cur_value[:-1]
+        box_label = int(hash(lens))
+        if lens in boxes[box_label]:
+            del boxes[box_label][lens]
+    else:
+        lens = cur_value[:-2]
+        focal = int(cur_value[-1])
+        box_label = int(hash(lens))
+        boxes[box_label][lens] = focal
+
+focusing_power = 0
+for box in boxes.keys():
+    lenses = list(boxes[box].keys())
+    focals = list(boxes[box].values())
+    for slot in range(len(lenses)):
+        focusing_power += (box + 1) * (slot + 1) * focals[slot]
+        print(lenses[slot], box + 1, slot + 1, focals[slot])
+
+print(focusing_power)
