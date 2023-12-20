998465c57e742ef3ec43b14905a92c4d5474af47
Marco <macto.14@gmail.com>
2023-12-20 17:38:24 -0300

    dia 19 parte 2

--- Day19/parts.py
+++ Day19/parts.py
@@ -22,6 +22,7 @@ for line in file[1].split("\n"):
     values = {k: int(v) for k, v in values}
     parts.append(values)
 
+# PART 1
 rating = 0
 for part in parts:
     accepted = False
@@ -46,4 +47,53 @@ for part in parts:
     if accepted:
         rating += x + m + a + s
 
-print(rating)
+print("PART 1:", rating)
+
+
+# PART 2
+def calc_combs(ranges: dict[str, tuple]) -> int:
+    combs = 1
+    for min, max in ranges.values():
+        if max > min:
+            combs *= max - min + 1
+    if combs == 1:
+        return 0
+    return combs
+
+
+def workflow(ranges: dict[str, tuple], cur_wf) -> int:
+    conds = workflows[cur_wf]["conds"]
+    default = workflows[cur_wf]["dft"]
+    combs = 0
+
+    for cond_eval, cond_wf in conds:
+        passed_ranges = dict(ranges)
+        denied_ranges = dict(ranges)
+
+        if "<" in cond_eval:
+            var, cut_val = cond_eval.split("<")
+            passed_ranges[var] = (passed_ranges[var][0], int(cut_val) - 1)
+            denied_ranges[var] = (int(cut_val), denied_ranges[var][1])
+
+        else:
+            var, cut_val = cond_eval.split(">")
+            passed_ranges[var] = (int(cut_val) + 1, passed_ranges[var][1])
+            denied_ranges[var] = (denied_ranges[var][0], int(cut_val))
+
+        if cond_wf == "A":
+            combs += calc_combs(passed_ranges)
+        elif cond_wf != "R":
+            combs += workflow(passed_ranges, cond_wf)
+
+        ranges = denied_ranges
+
+    if default == "A":
+        combs += calc_combs(ranges)
+    elif default != "R":
+        combs += workflow(ranges, default)
+    return combs
+
+
+ranges = {"x": (1, 4000), "m": (1, 4000), "a": (1, 4000), "s": (1, 4000)}
+
+print("PART 2:", workflow(ranges, "in"))
