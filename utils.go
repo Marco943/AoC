2e1dfa5b0e49119eee9cf6e4e92a83e7526820ea
Marco <macto.14@gmail.com>
2023-12-21 21:26:07 -0300

    add modulo e struct de grid

---
+++ utils.go
@@ -0,0 +1,64 @@
+package aoc
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Grid struct {
+	Data [][]string
+	W    int
+	H    int
+}
+
+func NewGrid(path string) *Grid {
+	file, err := os.Open(path)
+	if err != nil {
+		panic("erro na leitura do arquivo")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanRunes)
+
+	var grid [][]string
+
+	var line int = 0
+	grid = append(grid, []string{})
+	for scanner.Scan() {
+		char := scanner.Text()
+		if char == "\n" {
+			line++
+			grid = append(grid, []string{})
+			continue
+		} else if char != "\r" {
+			grid[line] = append(grid[line], char)
+		}
+	}
+
+	file.Close()
+
+	var h int = len(grid)
+	var w int = len(grid[0])
+	return &Grid{grid, w, h}
+}
+
+func (g Grid) Get(x int, y int) string {
+	return g.Data[y][x]
+}
+
+func (g Grid) Row(y int) []string {
+	return g.Data[y]
+}
+
+func (g Grid) Print() {
+	fmt.Println()
+	for y := 0; y < g.H; y++ {
+		fmt.Println(g.Row(y))
+	}
+	fmt.Println()
+}
+
+func (g Grid) Set(x int, y int, v string) {
+	g.Data[y][x] = v
+}
