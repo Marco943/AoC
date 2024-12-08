63fffb7ff813f1f87fd31f2ea7f6f19f5fb6e1d8
Marco <macto.14@gmail.com>
2024-12-07 22:41:59 -0300

    dia 5 parte 2

--- 2024/05/2405.py
+++ 2024/05/2405.py
@@ -33,10 +33,43 @@ def part_1(path: str) -> None:
 
 
 def part_2(path: str) -> None:
-    pass
+    with open(path, "r") as f:
+        lines = f.read().strip()
+    rules: list[tuple[int, int]] = []
+    updates: list[list[int]] = []
+
+    rules_lines, updates_lines = lines.split("\n\n", 1)
+
+    for line in rules_lines.split("\n"):
+        page_1, page_2 = line.split("|", 1)
+        rules.append((int(page_1), int(page_2)))
+
+    for line in updates_lines.split("\n"):
+        updates.append(list(map(int, line.split(","))))
+
+    def is_update_valid(update: list[int]) -> bool:
+        for n1, n2 in rules:
+            if (n1 in update) and (n2 in update):
+                i1 = update.index(n1)
+                i2 = update.index(n2)
+                if i1 > i2:
+                    update[i1], update[i2] = n2, n1
+                    return False
+        return True
+
+    result = 0
+    for update in updates:
+        if is_update_valid(update):
+            continue
+        while True:
+            if is_update_valid(update):
+                break
+        result += update[len(update) // 2]
+
+    print(result)
 
 
 part_1(f"{YEAR}/{DAY:0>2}/test.txt")
 part_1(f"{YEAR}/{DAY:0>2}/input.txt")
-# part_2(f"{YEAR}/{DAY:0>2}/test.txt")
-# part_2(f"{YEAR}/{DAY:0>2}/input.txt")
+part_2(f"{YEAR}/{DAY:0>2}/test.txt")
+part_2(f"{YEAR}/{DAY:0>2}/input.txt")
