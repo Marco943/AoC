590f6e3720b1f948a8dc982433244942e219501b
Marco <marco@sucessoemvendas.com.br>
2024-12-12 15:45:18 -0300

    dia 9 parte 1 otimizado

--- 2024/09/2409.py
+++ 2024/09/2409.py
@@ -1,6 +1,34 @@
 def part_1(path: str):
-    disk: list[int] = map(
-        lambda b: -1 if b == "." else int(b), list(open(path, "r").read().strip())
+    disk: list[int] = [int(x) for x in open(path, "r").read().strip()]
+
+    i = 0
+    j = len(disk) - 1
+    blocks: list[int] = []
+
+    while i < len(disk):
+        if disk[i] == 0:
+            i += 1
+            continue
+        if i % 2 == 0:
+            blocks.append(i // 2)
+        else:
+            while j % 2 != 0 or disk[j] == 0:
+                j -= 1
+            if j > 0:
+                blocks.append(j // 2)
+                disk[j] -= 1
+        disk[i] -= 1
+
+    checksum = 0
+    for i, block in enumerate(blocks):
+        checksum += i * block
+
+    return checksum
+
+
+def part_2(path: str):
+    disk: list[int] = list(
+        map(lambda b: -1 if b == "." else int(b), list(open(path, "r").read().strip()))
     )
     # disk =[ 1,2,3,4,5]
     id: int = 0
@@ -11,35 +39,4 @@ def part_1(path: str):
             continue
         blocks.extend([id] * block)
         id += 1
-
-    is_there_gaps: bool = True
-    first_free: int = 0
-    last_block: int = len(blocks) - 1
-    while is_there_gaps:
-        first_free = blocks.index(-1, first_free)
-
-        for block in blocks[first_free:]:
-            if block != -1:
-                break
-        else:
-            is_there_gaps = False
-            continue
-
-        for i in range(last_block, 0, -1):
-            if blocks[i] == -1:
-                continue
-            blocks[first_free], blocks[i] = blocks[i], -1
-            break
-
-        last_block = i
-
-        checksum: int = 0
-    for i, block in enumerate(blocks):
-        if block == -1:
-            break
-        checksum += i * block
-    return checksum
-
-
-def part_2(path: str):
     return
