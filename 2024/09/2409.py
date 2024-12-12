6f2a0c0bbbcb60d727b4ff5cbf31fb4402ff12fa
Marco <macto.14@gmail.com>
2024-12-12 20:41:49 -0300

    dia 9 parte 1 otimizado mais

--- 2024/09/2409.py
+++ 2024/09/2409.py
@@ -5,19 +5,21 @@ def part_1(path: str):
     j = len(disk) - 1
     blocks: list[int] = []
 
-    while i < len(disk):
+    while i <= j:
         if disk[i] == 0:
             i += 1
             continue
         if i % 2 == 0:
-            blocks.append(i // 2)
+            blocks.extend([i // 2] * disk[i])
+            disk[i] = 0
+            i += 1
         else:
             while j % 2 != 0 or disk[j] == 0:
                 j -= 1
             if j > 0:
                 blocks.append(j // 2)
                 disk[j] -= 1
-        disk[i] -= 1
+            disk[i] -= 1
 
     checksum = 0
     for i, block in enumerate(blocks):
@@ -30,16 +32,25 @@ def part_2(path: str):
     disk: list[int] = [int(x) for x in open(path, "r").read().strip()]
 
     i = 0
+    j = len(disk) - 1
     blocks: list[int] = []
 
-    for i in range(len(disk)):
+    while i <= j:
+        if disk[i] == 0:
+            i += 1
+            continue
         if i % 2 == 0:
             blocks.extend([i // 2] * disk[i])
+            disk[i] = 0
+            i += 1
         else:
-            blocks.extend([-1] * disk[i])
+            while j % 2 != 0 or disk[j] == 0:
+                j -= 1
 
-    for i in range(len(disk) - 1, 0, -1):
-        break
+            if j > 0:
+                blocks.append(j // 2)
+                disk[j] -= 1
+            disk[i] -= 1
 
     checksum = 0
     for i, block in enumerate(blocks):
