a55b3d6f0cd9b2c79e6ea31ed446c4ce090f5111
Marco <marco@sucessoemvendas.com.br>
2024-12-13 10:37:51 -0300

    calcula o tempo para completar o puzzle

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from argparse import ArgumentParser
 from importlib import import_module
 
@@ -12,13 +13,24 @@ YEAR: int = args.year
 DAY: int = args.day
 TEST = args.test
 
+
+def timeit(funcao):
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        r = funcao(*args, **kwargs)
+        dt = time.time() - t1
+        return f"[{dt*1000:.2f} ms] {r}"
+
+    return wrapper
+
+
 module = import_module(f"{YEAR}.{DAY:0>2}.{YEAR[-2:]}{DAY:0>2}")
 
 print("=" * 20 + " PART 1 " + "=" * 20)
-print("[Test]", module.part_1(f"{YEAR}/{DAY:0>2}/test.txt"))
+print("[Test]", timeit(module.part_1)(f"{YEAR}/{DAY:0>2}/test.txt"))
 if not TEST:
-    print("[Input]", module.part_1(f"{YEAR}/{DAY:0>2}/input.txt"))
+    print("[Input]", timeit(module.part_1)(f"{YEAR}/{DAY:0>2}/input.txt"))
 print("=" * 20 + " PART 2 " + "=" * 20)
-print("[Test]", module.part_2(f"{YEAR}/{DAY:0>2}/test.txt"))
+print("[Test]", timeit(module.part_2)(f"{YEAR}/{DAY:0>2}/test.txt"))
 if not TEST:
-    print("[Input]", module.part_2(f"{YEAR}/{DAY:0>2}/input.txt"))
+    print("[Input]", timeit(module.part_2)(f"{YEAR}/{DAY:0>2}/input.txt"))
